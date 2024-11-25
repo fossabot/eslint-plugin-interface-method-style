5d2036c8bee7e2900bc7fbdb9d1a333fa572ace0
Egor Avakumov <avakymov21@gmail.com>
2024-11-25 23:03:58 +0300

    feat: add ESLint configuration, CI/CD workflows, and improve README documentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,19 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.json",
-    tsconfigRootDir: __dirname,
-    ecmaVersion: "latest",
+    ecmaVersion: 2020,
     sourceType: "module",
   },
   plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+  },
   env: {
     node: true,
     es6: true,
